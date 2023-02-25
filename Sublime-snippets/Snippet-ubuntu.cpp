3c19e86bef07d938d906efa3f23cde5c6a099c2c
Shohan093 <102477787+Shohan093@users.noreply.github.com>
2023-02-25 14:38:12 +0600

    Snippet for Sublime Text

---
+++ Sublime-snippets/Snippet-ubuntu.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define huge unsigned long long int
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define sz(a) (int)a.size()
+#define all(a) a.begin(), a.end()
+#define nl '\n'
+
+const int32_t inf = 1e9;
+const int INF = 1e18;
+
+void loop_check(std::vector<int> v) { for(int x : v) cout << x << ' '; }
+
+void solve() {
+
+}
+
+int32_t main() {
+
+	fast_io
+
+	int test = 1;
+	cin >> test;
+	for(int test_case = 1; test_case <= test; ++test_case) {
+		cout << "Case " << test_case << ": ";
+		solve();
+	}
+	return 0;
+}
